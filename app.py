625e5c618b27e8ff618f67d92c1c2bb3fd01b631
Michael-Lc <michaellc143@gmail.com>
2021-07-10 17:00:22 +0000

    Added graph title, labels and color options 	- Deleted unnecessary test.db 	- Added title, label and color form options 	- Created "formatting" dict for hist argument containing title, color 	  and labels

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request
 
-# from flask_sqlalchemy import SQLAlchemy
 from modules.graph import histogram
 import numpy as np
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
-# db = SQLAlchemy(app)
 
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     seperators = [" ", ", ", ",", "\t"]
+
     if request.method == "POST":
         data = request.form["data"]
+        formating = {
+            "color": request.form["graph_color"],
+            "xlabel": request.form["xlabel"],
+            "ylabel": request.form["ylabel"],
+            "title": request.form["title"],
+        }
+
         try:
             interval = float(request.form["class-interval"])
         except:
-            interval = None  # defaul value
+            interval = None  # default value
 
         for sep in seperators:
             if sep in data:
                 datalist = data.split(sep)
                 print(datalist)
 
-        graph = histogram(datalist, interval=interval)
+        graph = histogram(datalist, formating=formating, interval=interval)
+        return render_template(
+            "index.html", graph=graph, data=data, interval=interval, formating=formating
+        )
     else:
         graph = histogram(list=np.random.uniform(0, 10, 50))
-    return render_template("index.html", graph=graph, data=data, interval=interval)
+        formating = {
+            "color": "",
+            "xlabel": "",
+            "ylabel": "",
+            "title": "",
+        }
+        return render_template("index.html", graph=graph, formating=formating)
 
 
 if __name__ == "__main__":
--- modules/graph.py
+++ modules/graph.py
@@ -4,21 +4,32 @@ from random import randint
 import math
 
 
-def histogram(list, interval=None, file=""):
+def histogram(list, formating={}, interval=None):
     # Generate the figure **without using pyplot**.
     fig = Figure()
     ax = fig.subplots()
     df = pd.DataFrame(list, dtype=float)
-    if not interval:
-        numofbins = interval
-    else:
+    numofbins = interval
+    range = None
+    color = None
+
+    if interval != None:
         xmax = math.ceil(df.max())
         xmin = math.floor(df.min())
         print("xmax: " + str(xmax))
+        range = (xmin, xmax)
         numofbins = int((xmax - xmin) / interval)
 
     print("number of bins: " + str(numofbins))
-    ax.hist(df, bins=numofbins, range=(xmin, xmax))
+
+    if len(formating) != 0:
+        ax.set_xlabel(formating["xlabel"])
+        ax.set_ylabel(formating["ylabel"])
+        fig.suptitle(formating["title"])
+        if formating["color"] != "":
+            color = formating["color"]
+
+    ax.hist(df, bins=numofbins, range=range, color=color)
     # hash = randint(1, 1000)
     # path = f"./static/graphs/histogram{hash}.png"
     path = f"./static/graphs/histogram.png"
