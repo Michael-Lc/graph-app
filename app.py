46a024bc2fdd22d9397d6ef669f992855c347baa
Michael-Lc <michaellc143@gmail.com>
2021-07-14 16:27:07 +0000

    Refactoring 	- Consisting naming for variables and form-data 	- Removed unnecessary variables 	- Added few descriptive comments

--- app.py
+++ app.py
@@ -8,40 +8,39 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    seperators = [" ", ", ", ",", "\t"]
 
     if request.method == "POST":
+        seperators = [" ", ", ", ",", "\t"]  # list of data seperators
         data = request.form["data"]
-        formating = {
-            "color": request.form["graph_color"],
-            "xlabel": request.form["xlabel"],
-            "ylabel": request.form["ylabel"],
-            "title": request.form["title"],
-        }
+        print(request.form)
 
+        # Cast class interval to float
+        # else set default value of None
         try:
             interval = float(request.form["class-interval"])
         except:
             interval = None  # default value
 
+        # Data splitting
         for sep in seperators:
             if sep in data:
                 datalist = data.split(sep)
                 print(datalist)
 
-        graph = histogram(datalist, formating=formating, interval=interval)
-        return render_template(
-            "index.html", graph=graph, data=data, interval=interval, formating=formating
-        )
+        graph = histogram(list=datalist, interval=interval, args=request.form)
+        return render_template("index.html", graph=graph, data=request.form)
     else:
         graph = histogram(list=np.random.uniform(0, 10, 50))
-        formating = {
-            "color": "",
-            "xlabel": "",
-            "ylabel": "",
-            "title": "",
-        }
-        return render_template("index.html", graph=graph, formating=formating)
+        # default form data values
+        data = [
+            ("data", ""),
+            ("class-interval", ""),
+            ("title", ""),
+            ("xlabel", ""),
+            ("ylabel", ""),
+            ("graph_color", ""),
+        ]
+        return render_template("index.html", graph=graph, data=data)
 
 
 if __name__ == "__main__":
--- modules/graph.py
+++ modules/graph.py
@@ -4,30 +4,32 @@ from random import randint
 import math
 
 
-def histogram(list, formating={}, interval=None):
+def histogram(list, interval=None, args=[]):
     # Generate the figure **without using pyplot**.
     fig = Figure()
     ax = fig.subplots()
     df = pd.DataFrame(list, dtype=float)
-    numofbins = interval
+    # default values
+    numofbins = None
     range = None
     color = None
 
+    # calculate number of bins if interval is available
     if interval != None:
-        xmax = math.ceil(df.max())
-        xmin = math.floor(df.min())
-        print("xmax: " + str(xmax))
+        xmax, xmin = math.ceil(df.max()), math.floor(df.min())
+        # print("xmax: " + str(xmax))
         range = (xmin, xmax)
         numofbins = int((xmax - xmin) / interval)
 
-    print("number of bins: " + str(numofbins))
+    # print("number of bins: " + str(numofbins))
 
-    if len(formating) != 0:
-        ax.set_xlabel(formating["xlabel"])
-        ax.set_ylabel(formating["ylabel"])
-        fig.suptitle(formating["title"])
-        if formating["color"] != "":
-            color = formating["color"]
+    # set graph formating if values are available
+    if len(args) != 0:
+        ax.set_xlabel(args["xlabel"])
+        ax.set_ylabel(args["ylabel"])
+        fig.suptitle(args["title"])
+        if args["graph_color"] != "":
+            color = args["graph_color"]
 
     ax.hist(df, bins=numofbins, range=range, color=color)
     # hash = randint(1, 1000)
